fe1e8c9c9df8d01442a7c0bf7bbf84c7eade3678
vorapat0881 <vorapat_g@cmu.ac.th>
2023-12-01 15:52:35 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x; //ประกาศตัวแปร x
+    int even = 0, odd = 0; //ตั้งค่าเริ่มต้นของตัวแปร even กับ odd เพื่อนับจำนวนตัวเลขคู่และคี่
+    while(x != 0){ //ตราบใดที่ x ไม่ได้มีค่าเป็น 0 (โปรแกรมจะออกลูปถ้าใส่เลข 0)
+        cout << "Enter an integer: "; //พิมพ์ข้อความว่า "Enter an integer: " อีกรอบ 
+        cin >> x; //รับค่าตัวแปร x จากผู้ใช้อีกรอบ
+        if(x%2 == 0 ){ //ถ้า x หารสองลงตัว
+            if(x != 0) even = even+1; //เพิ่มค่า even ไป 1 
+        }else{
+            odd = odd+1; //เพิ่มค่า odd ไป 1 
+        }
+    }
+    cout << "#Even numbers = " << even << "\n"; //พิมพ์ค่าตัวแปร even และขึ้นบรรทัดใหม่
+    cout << "#Odd numbers = " << odd; //พิมพ์ค่าตัวแปร odd 
     return 0;
 }
